2f867117a5763d0718891b2d598ebde7650c4f37
vitalisolovchuk <vitalisolovchuk@gmail.com>
2019-11-27 00:36:43 +0300

    created new project, gitignore, added Book model

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
