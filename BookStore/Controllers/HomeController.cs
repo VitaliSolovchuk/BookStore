5ce8bf6a170342a7ed843d3b385fec1e3756f912
VitaliSolovchuk <vitalisolovchuk@gmail.com>
2019-11-27 13:12:46 +0300

    added Purchase model, created Context for two models and added his initializer. Creadet test controller and View.

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using BookStore.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Book> iBooksContext = db.Books;
+            ViewBag.Message = "you see books";
+            return View(iBooksContext);
         }
 
         public ActionResult About()
---
+++ BookStore/Models/BookContextInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public class BookContextInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { Name = "Источник", Author = "Айн Рэнд", Price = 99 });
+            context.Books.Add(new Book { Name = "Похождение скверной девченки", Author = "Марио Вергас Льоса", Price = 59 });
+            context.Books.Add(new Book { Name = "Город перестановок", Author = "Грег Иган", Price = 35 });
+
+            base.Seed(context);
+        }
+    }
+}
